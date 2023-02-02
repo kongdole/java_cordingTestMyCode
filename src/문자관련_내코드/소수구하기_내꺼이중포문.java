225157de1dd3660e19140d5eca9c76309a2d1bd6
kongdole <rbgkswjs@gmail.com>
2023-02-02 09:24:42 +0900

    소수구하기_내코드이중포문

--- src/문자관련_내코드/소수구하기_내꺼이중포문.java
+++ src/문자관련_내코드/소수구하기_내꺼이중포문.java
@@ -3,30 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+	
 	public int solution(int num) {
-		int answer = 0;
-		int[] arr = new int[num + 1];
-		for (int i = 2; i <= num; i++) {
-			if (arr[i] == 0) {
-				answer++;
-				for (int j = i; j <= num; j =j+i) {
-					arr[j] = 1;
+		int answer=0;
+		for(int i =2; i<=num;i++) {
+			int cnt=0;
+			for(int j =1; j<i; j++) {
+				if(i%j==0) {
+					cnt++;
 				}
 			}
+			if(cnt==1) {
+				answer++;
+			}
 		}
-
+		System.out.println();
 		return answer;
 	}
-
+	
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+	
+		
 		Main T = new Main();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
 		long start = System.currentTimeMillis();
 		System.out.println(T.solution(num));
 		long end = System.currentTimeMillis();
-		System.out.println("SDB에서 노드생성까지의 실행시간 : " + (end - start) / 1000.0);
+		System.out.println("출력하는데 걸린시간 : " + (end - start)/1000.0);
 	}
 }
